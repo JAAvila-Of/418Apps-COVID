05a73a64fafcb33e6c474b6621843cfec75a9989
Jose Angel Avila <j2ab.jaavila@gmail.com>
2021-04-06 18:58:52 -0500

    añadiendo configuración SEO

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,37 +1,8 @@
 <script lang="ts">
   import { Router, Route } from "svelte-routing";
-  import SvelteSeo from "svelte-seo";
   import Home from "../pages/home.svelte";
   import Error from "../pages/p404.svelte";
 </script>
-
-<SvelteSeo
-  description="Mira el estado de la vacunación contra el Covid-19 en el Perú con datos oficiales del gobierno."
-  twitter={{
-    site: "@JoseAngelAvil18",
-    creator: "@JoseAngelAvil18",
-    title: "Vacunación Covid-19 en Perú",
-    description:
-      "Mira el estado de la vacunación contra el Covid-19 en el Perú con datos oficiales del gobierno.",
-    image: "https://418-apps-covid.vercel.app/og.jpg",
-    imageAlt: "Portada de App",
-  }}
-  openGraph={{
-    title: "Vacunación Covid-19 en Perú",
-    description:
-      "Mira el estado de la vacunación contra el Covid-19 en el Perú con datos oficiales del gobierno.",
-    type: "article",
-    url: "https://418-apps-covid.vercel.app",
-    images: [
-      {
-        url: "https://418-apps-covid.vercel.app/og.jpg",
-        width: 1200,
-        height: 627,
-        alt: "progreso_de_la_vacunacion_peru",
-      },
-    ],
-  }}
-/>
 
 <Router>
   <Route path="*">
