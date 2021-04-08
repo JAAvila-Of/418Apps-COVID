f6c71c71783ad61d3e8d0d7a90622d168be2ca69
Jose Angel Avila <j2ab.jaavila@gmail.com>
2021-04-07 19:45:13 -0500

    mejorando cache

--- workbox-config.js
+++ workbox-config.js
@@ -1,5 +1,84 @@
 module.exports = {
   globDirectory: "public/",
-  globPatterns: ["**/*.{svg,png,css,js,json,woff,woff2,html,jpg}"],
+  globPatterns: ["**/*.{css,js,html}"],
   swDest: "public/sw.js",
+  clientsClaim: true,
+  skipWaiting: true,
+  importScripts: [],
+  globIgnores: ["public/build/data/*", "og.jpg"],
+  runtimeCaching: [
+    {
+      urlPattern: "/",
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "start-url",
+        expiration: {
+          maxEntries: 1,
+          maxAgeSeconds: 86400,
+          purgeOnQuotaError: !0,
+        },
+      },
+    },
+    {
+      urlPattern: /\.(?:jpg|jpeg|gif|png|svg|ico|webp)$/i,
+      handler: "StaleWhileRevalidate",
+      options: {
+        cacheName: "static-image-assets",
+        expiration: {
+          maxEntries: 64,
+          maxAgeSeconds: 86400,
+          purgeOnQuotaError: !0,
+        },
+      },
+    },
+    {
+      urlPattern: /\.(?:eot|otf|ttc|ttf|woff|woff2|font.css)$/i,
+      handler: "CacheFirst",
+      options: {
+        cacheName: "static-font-assets",
+        expiration: {
+          maxEntries: 10,
+          maxAgeSeconds: 31536000,
+          purgeOnQuotaError: !0,
+        },
+      },
+    },
+    {
+      urlPattern: /\.(?:js)$/i,
+      handler: "StaleWhileRevalidate",
+      options: {
+        cacheName: "static-js-assets",
+        expiration: {
+          maxEntries: 5,
+          maxAgeSeconds: 86400,
+          purgeOnQuotaError: !0,
+        },
+      },
+    },
+    {
+      urlPattern: /\.(?:css|less)$/i,
+      handler: "StaleWhileRevalidate",
+      options: {
+        cacheName: "static-style-assets",
+        expiration: {
+          maxEntries: 5,
+          maxAgeSeconds: 86400,
+          purgeOnQuotaError: !0,
+        },
+      },
+    },
+    {
+      urlPattern: /.*/i,
+      handler: "NetworkFirst",
+      options: {
+        cacheName: "static-others-assets",
+        networkTimeoutSeconds: 10,
+        expiration: {
+          maxEntries: 32,
+          maxAgeSeconds: 86400,
+          purgeOnQuotaError: !0,
+        },
+      },
+    },
+  ],
 };
